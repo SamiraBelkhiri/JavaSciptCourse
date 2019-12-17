fa98674b8946d4f75a93fab8eb0cc46e38402ce4
SamiraBelkhiri <belkhirisamira@hotmail.com>
2019-12-17 17:01:48 +0100

    finally finished the calculator yeeeeeeeeeyyyyyy

---
+++ 02-maths/01-calculator-one/script.js
@@ -0,0 +1,47 @@
+/* becode/javascript
+ *
+ * /02-maths/01-calculator-one/script.js - 2.1: calculatrice
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+    // to get the value of an input: document.getElementById("element-id").value
+
+    document.getElementById("addition").addEventListener("click", function() {
+        // perform an addition
+        let one = parseInt(document.getElementById("op-one").value);
+        let two = parseInt(document.getElementById("op-two").value);
+        alert(one+two);
+    });
+
+    document.getElementById("substraction").addEventListener("click", function() {
+        // perform an substraction
+        let one = parseInt(document.getElementById("op-one").value);
+        let two = parseInt(document.getElementById("op-two").value);
+        alert(one-two);
+    });
+
+    document.getElementById("multiplication").addEventListener("click", function() {
+        // perform an multiplication
+        let one = parseInt(document.getElementById("op-one").value);
+        let two = parseInt(document.getElementById("op-two").value);
+        alert(one*two);
+    });
+
+    document.getElementById("division").addEventListener("click", function() {
+        // perform an division
+        let one = parseInt(document.getElementById("op-one").value);
+        let two = parseInt(document.getElementById("op-two").value);
+        alert(one/two);
+    });
+})();
+
+
+
+
+
