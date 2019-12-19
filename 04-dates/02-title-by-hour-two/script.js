0390d5a0b908358da4e707929fa5d732d4341020
SamiraBelkhiri <belkhirisamira@hotmail.com>
2019-12-19 12:55:05 +0100

    changed the numbers to form 17:30

--- 04-dates/02-title-by-hour-two/script.js
+++ 04-dates/02-title-by-hour-two/script.js
@@ -9,10 +9,22 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // your code here
+function () {
+
+    var time =new Date ().getHours();
+    if (time < 18 && time > 5){
+        document.getElementById("target").innerHTML = ("hello");
+
+}
+    else {
+        document.getElementById("target").innerHTML = ("good evening");
+
+    }
+
+
+
+
 
 })();
