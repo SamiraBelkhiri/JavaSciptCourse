67fcbf8a01b491ce30dd17e8b8b70d98ba85c648
SamiraBelkhiri <belkhirisamira@hotmail.com>
2019-12-20 11:28:16 +0100

    finished spooky friday  using push method to give alert

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -10,7 +10,32 @@
 // You will have time to focus on it later.
 
 (function() {
+// when you click
+    document.getElementById("run").addEventListener("click", function(){
 
-    // your code here
+        // get value
+        var value = document.getElementById("year").value;
+        var d= new Date();
+        // check for 13 in the setfullyear
+        d.setFullYear(value, 0,13);
+    // empty array to push answer to alert message in the web page
+         var months=[];
+        const mon =["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+        for (var m=0; m <= 11;m++){
+
+            d.setMonth(m);
+// gets friday to push it to the alert (in the array)
+            if ( d.getDay() === 5){
+                months.push(mon[m])
+            }
+        }
+
+        alert(months);
+
+
+
+
+    })
 
 })();
