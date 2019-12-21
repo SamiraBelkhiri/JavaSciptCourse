50f6b7e476075b9972c25c4cd3753365d91e06c1
SamiraBelkhiri <belkhirisamira@hotmail.com>
2019-12-21 22:12:56 +0100

    started with a loop that wouldnt stop so changed it to forEach

--- 05-arrays/04-walk-two/script.js
+++ 05-arrays/04-walk-two/script.js
@@ -10,22 +10,51 @@
 // You will have time to focus on it later.
 
 (function() {
+    document.getElementById("run").addEventListener("click", function () {
+        var index, len;
+        var fruits = [
+            "pomme",
+            "poire",
+            "fraise",
+            "tomate",
+            "kiwi",
+            "banane",
+            "orange",
+            "mandarine",
+            "durian",
+            "pêche",
+            "raisin",
+            "cerise",
+        ];
+      a.forEach(function (entry) {
+          console.log(entry);
+      });
 
-    var fruits = [
-        "pomme",
-        "poire",
-        "fraise",
-        "tomate",
-        "kiwi",
-        "banane",
-        "orange",
-        "mandarine",
-        "durian",
-        "pêche",
-        "raisin",
-        "cerise",
-    ];
+      })
 
-    // your code here
 
+      /*
+        //one way but not sure if its right
+        for (index= 0, len=fruits.length; index<len; ++index){
+
+            console.log(fruits[index]);
+        }
+
+ */
+ // other way but keeps counting so needs a break
+        /*
+         :   var counter = 0;
+            function next_word() {
+                console.log(fruits [counter % fruits.length]);
+                counter += 1;
+
+            }
+
+            setInterval(next_word, 100);
+        */
+
+
+// your code here
+
+//    })();
 })();
