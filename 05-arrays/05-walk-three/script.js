ca235a449d87713bd322e53dd5293cdb660f880f
SamiraBelkhiri <belkhirisamira@hotmail.com>
2019-12-21 22:56:04 +0100

    it prints names and email, cant seem to change it but will be trying later

--- 05-arrays/05-walk-three/script.js
+++ 05-arrays/05-walk-three/script.js
@@ -11,34 +11,45 @@
 
 (function() {
 
-    var people = [
-        {
-            firstname: "Garald",
-            lastname: "Freschi",
-            email: "gfreschi0@discuz.net",
-        },
-        {
-            firstname: "Bendicty",
-            lastname: "Foster-Smith",
-            email: "bfostersmith1@mysql.com",
-        },
-        {
-            firstname: "Michelle",
-            lastname: "Draude",
-            email: "mdraude2@csmonitor.com",
-        },
-        {
-            firstname: "Cathee",
-            lastname: "Meineck",
-            email: "cmeineck3@t-online.de",
-        },
-        {
-            firstname: "Marianna",
-            lastname: "Coal",
-            email: "mcoal4@creativecommons.org",
-        },
-    ];
+        var people = [
+            {
+                firstname: "Garald",
+                lastname: "Freschi",
+                email: "gfreschi0@discuz.net",
+            },
+            {
+                firstname: "Bendicty",
+                lastname: "Foster-Smith",
+                email: "bfostersmith1@mysql.com",
+            },
+            {
+                firstname: "Michelle",
+                lastname: "Draude",
+                email: "mdraude2@csmonitor.com",
+            },
+            {
+                firstname: "Cathee",
+                lastname: "Meineck",
+                email: "cmeineck3@t-online.de",
+            },
+            {
+                firstname: "Marianna",
+                lastname: "Coal",
+                email: "mcoal4@creativecommons.org",
+            },
+        ];
 
+    document.getElementById("run").addEventListener("click", function () {
+
+
+        people.forEach(function (item,index,array) {
+            console.log(item);
+
+        })
+
+
+    })
     // your code here
+
 
 })();
