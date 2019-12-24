49121f5a0e40b5068a331cf1b76b7f0e1490b484
SamiraBelkhiri <belkhirisamira@hotmail.com>
2019-12-24 12:09:40 +0100

    i got it to move to the next picture but i think its the last picture not next so i will get back to this one

--- 06-dom/06-mini-carousel/script.js
+++ 06-dom/06-mini-carousel/script.js
@@ -9,7 +9,7 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+
 
     var gallery= [
         "../../_shared/img/bell.svg",
@@ -19,6 +19,38 @@
         "../../_shared/img/map.svg",
     ];
 
+   /* function loadImagesInSequence(gallery) {
+        if (!gallery.length) {
+            return;
+        }
+
+        var img = new Image(),
+            url = gallery.shift();
+
+        img.onload = function(){ loadImagesInSequence(gallery) };
+        img.src = url;
+    }
+
+    loadImagesInSequence(['a.png', 'b.png', 'c.png']);
+*/
+   document.getElementById("next").addEventListener("click", myFunction);
+
+
+    function myFunction() {
+
+
+        for (var i = 0; i < gallery.length; i++) {
+
+
+
+            var x = document.getElementById("IMG");
+
+           x.setAttribute("src", gallery[i]);
+
+
+        }
+    }
+
     // your code here
 
-})();
+
