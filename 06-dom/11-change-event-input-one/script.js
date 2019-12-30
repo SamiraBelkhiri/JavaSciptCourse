95b75a8bea3551fa13354962e16d455acf9259c0
SamiraBelkhiri <belkhirisamira@hotmail.com>
2019-12-30 15:20:24 +0100

    yoehoewgit add 11-change-event-input-one/git add 11-change-event-input-one/

--- 06-dom/11-change-event-input-one/script.js
+++ 06-dom/11-change-event-input-one/script.js
@@ -9,8 +9,29 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+
+
+
+/*
+    function doThing(){
+        alert('Horray! Someone wrote "' + this.value + '"!');
+    }
+
+*/
+document.getElementById("pass-one").addEventListener('input', function() {
+
+
+    document.getElementById("pass-one").maxLength = "10";
+    var leng = document.getElementById("pass-one").value.length;
+    document.getElementById('counter').innerHTML = leng +"/10";
+
+
+});
+
+
+
 
     // your code here
 
-})();
+
+
