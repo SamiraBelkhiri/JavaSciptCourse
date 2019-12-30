5d3fe0c5ae18e7f128d916cb03953310bebb4fcf
SamiraBelkhiri <belkhirisamira@hotmail.com>
2019-12-30 14:22:49 +0100

    added class error to html through javascript

--- 06-dom/10-match-password-two/script.js
+++ 06-dom/10-match-password-two/script.js
@@ -9,8 +9,42 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+
+document.getElementById("run").addEventListener("click",checkPassword);
+
+
+function checkPassword() {
+
+    var passOne = document.getElementById("pass-one").value;
+    var passTwo = document.getElementById("pass-two").value;
+
+    // If password not entered
+    if (passOne === '') {
+        alert("Please enter Password");
+    }
+    // If confirm password not entered
+    else if (passTwo === '') {
+        alert("Please enter confirm password");
+    }
+    // If Not same return False.
+    else if (passOne !== passTwo) {
+        alert("\nPassword did not match: Please try again...");
+        document.getElementById("pass-one").style.borderColor = "red";
+        document.getElementById("pass-one").classList.add("error")
+        document.getElementById("pass-two").style.borderColor = "red";
+        document.getElementById("pass-two").classList.add("error")
+
+        return false;
+
+    }
+
+    // If same return True.
+    else {
+        alert("Password Match: Welcome to Becode!");
+        return true;
+    }
+}
+
 
     // your code here
 
-})();
