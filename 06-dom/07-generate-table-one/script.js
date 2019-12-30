05651d99bc2b47d2e5ddd8ad4d3f86db991aa366
SamiraBelkhiri <belkhirisamira@hotmail.com>
2019-12-30 10:46:51 +0100

    found a code on google that i could reuse to make tables in javascript

--- 06-dom/07-generate-table-one/script.js
+++ 06-dom/07-generate-table-one/script.js
@@ -11,6 +11,30 @@
 
 (function() {
 
+    function addTable() {
+        var myTableDiv = document.getElementById("target");
+
+        var table = document.createElement('TABLE');
+        table.border = '1';
+
+        var tableBody = document.createElement('TBODY');
+        table.appendChild(tableBody);
+
+        for (var i = 0; i < 1; i++) {
+            var tr = document.createElement('TR');
+            tableBody.appendChild(tr);
+
+            for (var j = 0; j < 10; j++) {
+                var td = document.createElement('TD');
+                td.width = '75';
+
+                tr.appendChild(td);
+            }
+        }
+        myTableDiv.appendChild(table);
+    }
+    addTable();
+
     // your code here
 
 })();
