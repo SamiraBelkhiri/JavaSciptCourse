28b9dcdd40a429e91aa029e211281d9baac56211
SamiraBelkhiri <belkhirisamira@hotmail.com>
2019-12-31 09:00:22 +0100

    got help from steve cause i couldnt get it to work but there is still something to change will work on it later

--- 06-dom/12-change-event-input-two/script.js
+++ 06-dom/12-change-event-input-two/script.js
@@ -9,8 +9,66 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+document.getElementById("pass-one").addEventListener('keydown', function() {
+  let input = document.getElementById("pass-one");
+  let validInput = document.getElementById("validity");
 
-})();
+
+  function CheckPassword() {
+
+    let twoNumbers = new RegExp(".*[0-9].*[0-9]");
+    return(input.value.length >= 7) && (twoNumbers.test(input.value))
+
+    }
+    if (CheckPassword()=== true){
+        validInput.innerHTML = "ok";
+
+    }else {
+        validInput.innerHTML = "not ok";
+    }
+
+
+
+    });
+
+
+/*
+
+    if ( (?=.{8,}) && newRegex.test(document.getElementById("pass-one").value)) {
+
+        document.getElementById('validity').innerHTML = "ok";
+    } else {
+        document.getElementById('validity').innerHTML = "not ok";
+    }
+*/
+
+/*
+myInput.onfocus = function() {
+    document.getElementById("pass-one").style.display = "block";
+}
+
+
+var numbers = /[0-9]/g;
+if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+} else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+}
+
+if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+} else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+}
+}
+
+*/
+
+
+
+
