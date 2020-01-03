8a4de085d8e5812c8a45ce46a116af64227a083b
SamiraBelkhiri <belkhirisamira@hotmail.com>
2020-01-03 10:39:40 +0100

    tried to change color with enter

---
+++ 03-colors/02-change-bcg-two/script.js
@@ -0,0 +1,26 @@
+/* becode/javascript
+ *
+ * /03-colors/02-change-bcg-two/script.js - 3.2: couleur de fond (2)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+
+/*
+document.getElementById("run").addEventListener("click", function () {
+    document.body.style.backgroundColor = document.getElementById("color").value;
+
+});
+
+*/
+
+document.getElementById('run').addEventListener('keypress', function (e) {
+    if (e.key === 13) {
+        // code for enter
+        document.body.style.backgroundColor = document.getElementById("color").value;
+    }
+});
