2bca11306c3b6c17db4455553863de4e19248d51
SamiraBelkhiri <belkhirisamira@hotmail.com>
2020-01-07 12:21:09 +0100

    got a lot of help from Ivo while i am home sick

--- 07-misc/05-worst-ui-three/script.js
+++ 07-misc/05-worst-ui-three/script.js
@@ -9,8 +9,78 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+(function () {
 
-})();
+    var boo = "460";
+    var meh = "00";
+    var sassy = "00";
+    var bleh = "00";
+
+    var speed = 100;
+    var speed2 = 80;
+    var setSonic = setInterval(sonic, speed);
+    var setTails = setInterval(tails, speed2);
+    var setKnuckles = setInterval(knuckles, speed);
+    var setAmy = setInterval(amy, speed2);
+
+    function sonic() {
+        var mario = Math.floor(Math.random() * (499 - 460) + 460);
+        document.getElementById("part-one").setAttribute("value", mario);
+    }
+
+    function tails() {
+        var wario = Math.floor(Math.random() * 100);
+        document.getElementById("part-two").setAttribute("value", wario);
+    }
+
+    function knuckles () {
+        var luigi = Math.floor(Math.random() * 100);
+        document.getElementById("part-three").setAttribute("value", luigi);
+    }
+
+    function amy () {
+        var toad = Math.floor(Math.random() * 100);
+        document.getElementById("part-four").setAttribute("value", toad);
+    }
+
+
+
+    document.getElementById("fix-part-two").addEventListener("click",function () {
+    clearInterval(setTails);
+    meh = (meh < 10)
+        {
+    meh ="0" + meh
+    }
+    document.getElementById("target").innerHTML = "+" + boo + meh + sassy +bleh;
+    });
+
+    document.getElementById("fix-part-three").addEventListener("click", function () {
+        clearInterval(setKnuckles);
+        sassy = document.getElementById("part-three").getAttribute("value");
+        if (sassy < 10) {
+            sassy = "0" + sassy
+        }
+        document.getElementById("target").innerHTML = "+" + boo + meh + sassy + bleh;
+
+    });
+
+    document.getElementById("fix-part-four").addEventListener("click", function (){
+        clearInterval(setAmy);
+        bleh = document.getElementById("part-four").getAttribute("value");
+        if (bleh < 10){
+            bleh = "0" + bleh}
+        document.getElementById("target").innerHTML = "+" + boo + meh + sassy + bleh;
+
+    });
+
+    })();
+
+
+
+
+
+
+
+
+
