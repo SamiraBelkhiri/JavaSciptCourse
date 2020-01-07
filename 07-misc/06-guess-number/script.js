5bbb0602efe61e7c5259aa64e3c180ebe4940bbc
SamiraBelkhiri <belkhirisamira@hotmail.com>
2020-01-07 12:52:56 +0100

    found some code on GeeksforGeeks and re-used it

--- 07-misc/06-guess-number/script.js
+++ 07-misc/06-guess-number/script.js
@@ -9,8 +9,60 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+// random value generated
+var y = Math.floor(Math.random() * 10 + 1);
 
-    // your code here
+// counting the number of guesses
+// made for correct Guess
+var guess = 1;
 
-})();
+document.getElementById("submitguess").onclick = function(){
+
+    // number guessed by user
+    var x = document.getElementById("guessField").value;
+
+    if(x == y)
+    {
+        alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
+            + guess + " GUESS ");
+    }
+    else if(x > y) /* if guessed number is greater
+                   than actual number*/
+    {
+        guess++;
+        alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+    }
+    else
+    {
+        guess++;
+        alert("OOPS SORRY!! TRY A GREATER NUMBER")
+    }
+}    // random value generated
+var y = Math.floor(Math.random() * 10 + 1);
+
+// counting the number of guesses
+// made for correct Guess
+var guess = 1;
+
+document.getElementById("submitguess").onclick = function(){
+
+    // number guessed by user
+    var x = document.getElementById("guessField").value;
+
+    if(x == y)
+    {
+        alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
+            + guess + " GUESS ");
+    }
+    else if(x > y) /* if guessed number is greater
+                   than actual number*/
+    {
+        guess++;
+        alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+    }
+    else
+    {
+        guess++;
+        alert("OOPS SORRY!! TRY A GREATER NUMBER")
+    }
+}
