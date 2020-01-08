15d4ae1f65a4aa6e7cd01b3ffd0197f131c876a3
SamiraBelkhiri <belkhirisamira@hotmail.com>
2020-01-08 16:35:56 +0100

    need to find the fetch host cant figure it out on my own

--- 09-fetch/01-list-to-console/script.js
+++ 09-fetch/01-list-to-console/script.js
@@ -9,18 +9,45 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+// need to know my localhost for the api
+/*
 (() => {
+document.getElementById("run").addEventListener("click", function () {
+
+    fetch("http://localhost:8083/api.json")
+        .then(response => {
+            return response.json()
+        })
+        .then(function (data) {
+            let names = data.heroes;
+            names.forEach(function (element) {
+                console.log(element.name);
+            })
+        })
+        .catch(err=> {
+        console.log("error")
+        })
+});
+
+        })();
+
+
+*/
+
+
+/*
+(function () {
+
+
 
     document.getElementById("run").addEventListener("click", function () {
         fetch('http://localhost:12345/api.json')
-            .then((response) => {
-                return response.json();
-            })
-            .then((myJson) => {
-                console.log(myJson);
+            .then(response => response.json())
+
+            .then (data => console.log(data))
             });
 
 
-    })
 
 })();
+*/
